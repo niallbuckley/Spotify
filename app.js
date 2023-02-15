4a546ea2296480206bba0b840d218687266f52b0
Niall Buckley <buckleyniall100@gmail.com>
2023-02-15 16:13:47 -0600

    Minor documentation

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs')
+// set that all templates are located in `/views` directory
+app.set('views', __dirname + '/views');
 
-app.use('/', require('./routes/login'))
-app.use('/', require('./routes/mode'))
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/login'));
+app.use('/', require('./routes/mode'));
 
 const PORT = process.env.PORT || 4111;
-app.listen(PORT, console.log("Server don start for port: " + PORT))
+app.listen(PORT, console.log("Server don start for port: " + PORT));
