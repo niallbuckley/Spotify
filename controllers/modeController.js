a4c3eb79b545cd84dc32b2e8a0f0e50ef437fa61
Niall Buckley <buckleyniall100@gmail.com>
2023-02-20 15:28:22 -0600

    clean up code

--- controllers/modeController.js
+++ controllers/modeController.js
@@ -15,7 +15,6 @@ const modeChoiceView = (req, res) => {
     var stateInDatabase = false;
     // checking if the request has cookies, if it does, what it checks for the auth state if it can't find either return null.
     var storedState = req.cookies ? req.cookies[stateKey] : null;
-    console.log("mode States: ", state, storedState, req.session);
     // Read the existing data from the file
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
@@ -23,57 +22,47 @@ const modeChoiceView = (req, res) => {
         return;
       }
 
+      // Read existing data from database.
       let jsonData = JSON.parse(data);
-      console.log("hello", jsonData);
 
       // Check if the key exists in the JSON data
       if (jsonData.hasOwnProperty(storedState)) {
         console.log("state in database.")
         stateInDatabase = true;
       }
-      console.log("state in data ", stateInDatabase);
+
       if ((state === null || state !== storedState) === true && stateInDatabase === false) {
+        console.log("REDIRECT");
         res.redirect('/#' +
            querystring.stringify({
               error: 'state_mismatch'
            }));
       }
       else {
-        // Define the file path
   
-        // Read the existing data from the file
-        fs.readFile(filePath, 'utf8', (err, data) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-  
-          let jsonData = JSON.parse(data);
-  
-          // Check if the key exists in the JSON data
-          if (jsonData.hasOwnProperty(state)) {
-            console.log('The key already exists in the JSON data.');
-            return;
-          }
-  
-          // If the key does not exist, add it to the JSON data
-          console.log("here");
+        // Check if the key exists in the JSON data
+        if (jsonData.hasOwnProperty(state)) {
+          console.log('The key already exists in the JSON data.');
+        }
+        else{
+          // If the key does not exist, add it to the database
+
           jsonData[state] = 'true'
           console.log(jsonData);
-  
+
           // Convert the JSON data to a string
           const jsonString = JSON.stringify(jsonData, null, 2);
-  
+
           // Write the updated data back to the file
           fs.writeFile(filePath, jsonString, 'utf8', (err) => {
             if (err) {
               console.error(err);
               return;
             }
-  
+
             console.log('The key was successfully added to the JSON data.');
           });
-        });
+        }
         // This is looking at views diretory 
         res.render("mode", {
         }); 
