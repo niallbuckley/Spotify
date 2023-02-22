6567b8c3235f6a58da768aa83f6edf795c8d6de6
Niall Buckley <buckleyniall100@gmail.com>
2023-02-22 17:12:34 -0600

    using different ports

--- controllers/hostLobbyController.js
+++ controllers/hostLobbyController.js
@@ -38,6 +38,33 @@ const hostLobbyView = (req, res) => {
         // This is looking at views diretory 
         res.render("hostLobby", {
         }); 
+        const WebSocket = require("ws");
+        const WebSocketServer = WebSocket.Server;
+
+        const server = new WebSocketServer({ port: 3000 });
+
+        console.log("Socket Server started");
+
+        server.on("connection", (socket) => {
+          // send a message to the client
+          socket.send(JSON.stringify({
+            type: "hello from server",
+            content: [ 1, "2" ]
+          }));
+
+          // receive a message from the client
+          socket.on("message", (data) => {
+            const packet = JSON.parse(data);
+
+            switch (packet.type) {
+              case "hello from client":
+                // ...
+                console.log("Server: message received from client")
+                //document.getElementById("messages").innerHTML = "Client Joined";
+                break;
+            }
+          });
+        });
       }
     });
 }
