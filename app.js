f39127b5e3f5e629b8b1c2c33da45904d05cd9b3
Niall Buckley <buckleyniall100@gmail.com>
2023-03-01 18:31:15 -0600

    Get the username using the cookie and reading from database

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './database.json');
+
 // set that all templates are located in `/views` directory
 app.set('views', __dirname + '/views');
 
@@ -11,8 +15,24 @@ app.use('/', require('./controllers/loginSpotifyController'));
 app.use('/', require('./routes/routes'));
 
 app.get('/spotify-username', (req, res) =>  {
-    console.log('HHHEEELLLOOO   ', req.cookies.spotify_auth_state);
     user = req.cookies.spotify_auth_state;
+    // Read the existing data from the database
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+  
+        let jsonData = JSON.parse(data);
+  
+        // Check if the key exists in the JSON data
+        if (jsonData.hasOwnProperty(user)) {
+            console.log("User_name: ", jsonData[user]);
+        }
+        else{
+            console.log("Error user not found in database.")
+        }
+    })
     res.send("Hello Client");
 
 })
