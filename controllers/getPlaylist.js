227ff406f0c040962b73858de1cc734580a10939
Niall Buckley <buckleyniall100@gmail.com>
2023-03-24 18:04:58 -0500

    build out the GET playlist logic

--- controllers/getPlaylist.js
+++ controllers/getPlaylist.js
@@ -5,9 +5,24 @@ const playlistDatabase = path.join(__dirname, '.././playlist-database.json');
 var stateKey = 'spotify_auth_state';
 
 var getPlaylist = function(req, res)  {
-    const playListId = req.params.playListId;
+    const playListId = req.params.id;
 
     console.log("WooHoo: ", playListId);
+    fs.readFile(playlistDatabase, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        let jsonData = JSON.parse(data);
+  
+        // Check if the key exists in the JSON data
+        if (!(jsonData.hasOwnProperty(playListId))) {
+            return res.json({"ERROR": "Playlist ID not found in database"})
+        }
+        console.log("Users: ", Object.keys(jsonData[playListId]).length);
+        // For time being make playlist with only 20 tracks.
+        users_in_session =  Object.keys(jsonData[playListId]).length;
+    })
     return res.json({"getPlaylist": "Testing"});
 
 };
