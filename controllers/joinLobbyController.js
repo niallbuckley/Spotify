6469bcf7155b86c5a5ff531326d9792779fd2e98
Niall Buckley <buckleyniall100@gmail.com>
2023-04-13 19:09:51 -0500

    get playlist supports redis server

--- controllers/joinLobbyController.js
+++ controllers/joinLobbyController.js
@@ -1,14 +1,11 @@
 var querystring = require('querystring');
 var stateKey = 'spotify_auth_state';
- 
-// Define the filepath
 
 const { getRedisClient } = require('./redisConnection');
 const client = getRedisClient();
 
 
 const joinLobbyView = async(req, res) => {
-    var stateInDatabase = false;
     // checking if the request has cookies, if it does, what it checks for the auth state if it can't find either return null.
     var storedState = req.cookies ? req.cookies[stateKey] : null;
 
