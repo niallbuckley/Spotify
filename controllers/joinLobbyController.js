e5ca9a128b7075da517a268d6d2e4a85eb74a8ed
Niall Buckley <buckleyniall100@gmail.com>
2023-06-17 15:44:48 -0500

    minor clean up join lobby

--- controllers/joinLobbyController.js
+++ controllers/joinLobbyController.js
@@ -10,16 +10,10 @@ var redirect_uri = process.env.SPOTIFY_REDIRECT_URI; // Your redirect uri
 
 const joinLobbyView = async(req, res) => {
     // checking if the request has cookies, if it does, what it checks for the auth state if it can't find either return null.
-    //var storedState = req.cookies ? req.cookies[stateKey] : null;
     var storedState = req.cookies && req.cookies[stateKey] ? req.cookies[stateKey] : "null";
 
-    console.log("HEERRREEEE1 " , null);
-    console.log("HEERRREEEE1 " , storedState);
-
     var r = await client.exists(storedState);
-    console.log("HEERRREEEE2")
     if (!r) {
-      console.log("HEERRREEEE3")
       // if not, send the loginSpotifyController to the user with the same params they used initally. 
       console.log("UNAUTH");
       generateRandomString = require('./generateId');
